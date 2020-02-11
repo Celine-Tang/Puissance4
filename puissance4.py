da4c19963d73d7e4f81b3908595ca5b92063667a
fefe713705 <ny.jeannot@eleve.egd.mg>
2020-02-11 13:26:43 +0100

    +3.4

--- puissance4.py
+++ puissance4.py
@@ -27,12 +27,21 @@ def coup_possible(gril,col):
         return False
     
 #3.4
-def jouer(gril, j, col):
-    if col == 0 :
-        if j == 1:
-            col = 1
-        else :
-            col = 2
+def jouer(gril,j,col):
+    if gril[5][col]==0:
+        gril[5][col]=j
+    elif gril[4][col]==0:
+        gril[4][col]=j
+    elif gril[3][col]==0:
+        gril[3][col]=j
+    elif gril[2][col]==0:
+        gril[2][col]=j
+    elif gril[1][col]==0:
+        gril[1][col]=j
+    elif gril[0][col]==0:
+        gril[0][col]=j
+    return gril    
+
             
 #3.5
 def horiz(gril,j,lig,col) :
@@ -40,7 +49,12 @@ def horiz(gril,j,lig,col) :
     if j == r :
         if gril[lig][col+1] == r and gril[lig][col+2] == r and gril[lig][col+3]:
             return True
+        
 
+#3.7
+def diag_haut(gril, j, lig, col):
+    
+    
 #3.9
 def victoire(gril, j):
     for j in gril:
